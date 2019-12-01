25baed8fdce983770076804e3dceb00446cdc15d
Denis Novikov <voothi@gmail.com>
2019-12-01 17:14:58 +0300

    Refactoring var r > resume

--- src/MainArray.java
+++ src/MainArray.java
@@ -11,7 +11,7 @@ public class MainArray {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        Resume r;
+        Resume resume;
         while (true) {
             System.out.print("Введите одну из команд - (list | save uuid | delete uuid | get uuid | clear | exit): ");
             String[] params = reader.readLine().trim().toLowerCase().split(" ");
@@ -31,9 +31,9 @@ public class MainArray {
                     System.out.println(ARRAY_STORAGE.size());
                     break;
                 case "save":
-                    r = new Resume();
-                    r.uuid = uuid;
-                    ARRAY_STORAGE.save(r);
+                    resume = new Resume();
+                    resume.uuid = uuid;
+                    ARRAY_STORAGE.save(resume);
                     printAll();
                     break;
                 case "delete":
@@ -56,7 +56,7 @@ public class MainArray {
         }
     }
 
-    static void printAll() {
+    private static void printAll() {
         Resume[] all = ARRAY_STORAGE.getAll();
         System.out.println("----------------------------");
         if (all.length == 0) {
