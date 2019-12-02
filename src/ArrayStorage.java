583355e2649b5b8e1783c1e41ccf07e74663f4d1
Denis Novikov <voothi@gmail.com>
2019-12-02 15:26:45 +0300

    ArrayStorage - Constructor - Implemented save(), size(), clear() - get in progress

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,75 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+    private int counterElementsOfStorage;
+    private Resume[] storage;
 
+    public ArrayStorage() {
+        counterElementsOfStorage = 0;
+        storage = new Resume[10];
+    }
+
+    /**
+     * Вставляет в конец массива принимаемый элемент
+     */
     void save(Resume resume) {
+        storage[counterElementsOfStorage] = resume;
+        counterElementsOfStorage++;
     }
 
+    /**
+     * Получение резюме из массива
+     *
+     * @param uuid принимает в качестве аргумента строку с идентификатором
+     * @return объект Resume
+     */
     Resume get(String uuid) {
-        return null;
+        int i;
+        for (i = 0; i < storage.length; i++) {
+            if (uuid.equals(storage[i].getUuid())) {
+                break;
+            }
+        }
+        return storage[i];
     }
 
+    /**
+     * После удаления элемента, происходит смещение элементов.
+     *
+     * @param uuid
+     */
     void delete(String uuid) {
+
     }
 
+    /**
+     * @return Size method returns the number of elements in the array,
+     * occupied by objects such Resume, with sequential filling,
+     * without holes (null).
+     */
     int size() {
-        return 0;
+        int sizeArrayStorageNotNull = 0;
+        for (int i = 0; i < storage.length; i++) {
+//            System.out.println(storage[i]);
+            if (storage[i] == null) {
+                continue;
+            }
+            sizeArrayStorageNotNull++;
+        }
+        return sizeArrayStorageNotNull;
     }
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-            return new Resume[0];
+        return new Resume[0];
     }
 }
