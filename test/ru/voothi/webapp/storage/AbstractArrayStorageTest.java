7497b3da98bf18304e567a0882e9b774645ab22c
Denis Novikov <voothi@gmail.com>
2019-12-19 16:46:01 +0300

    HW4 Test - init vars

--- test/ru/voothi/webapp/storage/AbstractArrayStorageTest.java
+++ test/ru/voothi/webapp/storage/AbstractArrayStorageTest.java
@@ -1,14 +1,23 @@
 package ru.voothi.webapp.storage;
 
 import org.junit.Test;
-
-import static org.junit.Assert.*;
+import ru.voothi.webapp.model.Resume;
 
 public class AbstractArrayStorageTest {
+    private Storage storage = new ArrayStorage();
+    private static final String UUID_1 = "uuid1";
+    private static final String UUID_2 = "uuid2";
+    private static final String UUID_3 = "uuid3";
+
+    public void setUp() {
+        storage.clear();
+        storage.save(new Resume(UUID_1));
+        storage.save(new Resume(UUID_2));
+        storage.save(new Resume(UUID_3));
+    }
 
     @Test
     public void save() {
-        throw new IllegalStateException();
     }
 
     @Test
