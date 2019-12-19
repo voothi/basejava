ae39f58b6aadbb2c3f47786a952bdf5e12814cac
Denis Novikov <voothi@gmail.com>
2019-12-19 16:37:17 +0300

    HW4 Test - throw new IllegalStateException();

---
+++ test/ru/voothi/webapp/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,37 @@
+package ru.voothi.webapp.storage;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AbstractArrayStorageTest {
+
+    @Test
+    public void save() {
+        throw new IllegalStateException();
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void size() {
+    }
+
+    @Test
+    public void clear() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+}
