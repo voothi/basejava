5934f11be70fdc48e53015e3a3e4108f085b1a35
Denis Novikov <voothi@gmail.com>
2019-12-24 18:48:06 +0300

    HW4 - Reflection

---
+++ src/ru/voothi/webapp/MainReflection.java
@@ -0,0 +1,22 @@
+package ru.voothi.webapp;
+
+import ru.voothi.webapp.model.Resume;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Resume resume = new Resume();
+        Class<? extends Resume> resumeClass = resume.getClass();
+        Field field = resumeClass.getDeclaredFields()[0];
+        field.setAccessible(true);
+        System.out.println(field.getName());
+        System.out.println(field.get(resume));
+        field.set(resume, "uuid_new");
+        Method method = resumeClass.getMethod("toString");
+        Object result = method.invoke(resume);
+        System.out.println(result);
+    }
+}
