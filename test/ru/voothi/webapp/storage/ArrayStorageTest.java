02f6b69a5ad6d95737878bff1dda33be8b9e40cd
Denis Novikov <voothi@gmail.com>
2019-12-25 13:48:37 +0300

    HW4 + удали неиспользуемые импорты

--- test/ru/voothi/webapp/storage/ArrayStorageTest.java
+++ test/ru/voothi/webapp/storage/ArrayStorageTest.java
@@ -1,7 +1,5 @@
 package ru.voothi.webapp.storage;
 
-import static org.junit.Assert.*;
-
 public class ArrayStorageTest extends AbstractArrayStorageTest {
     public ArrayStorageTest() {
         super(new ArrayStorage());
--- test/ru/voothi/webapp/storage/SortedArrayStorageTest.java
+++ test/ru/voothi/webapp/storage/SortedArrayStorageTest.java
@@ -1,7 +1,5 @@
 package ru.voothi.webapp.storage;
 
-import static org.junit.Assert.*;
-
 public class SortedArrayStorageTest extends AbstractArrayStorageTest {
     public SortedArrayStorageTest() {
         super(new SortedArrayStorage());
