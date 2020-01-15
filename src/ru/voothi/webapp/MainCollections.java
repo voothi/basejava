d8cc9ef2476d16459b0d2dd51aea7f5988f10625
Denis Novikov <voothi@gmail.com>
2020-01-15 15:26:15 +0300

    MainCollections - Test Collection

--- src/ru/voothi/webapp/MainCollections.java
+++ src/ru/voothi/webapp/MainCollections.java
@@ -1,7 +1,35 @@
 package ru.voothi.webapp;
 
-public class MainCollections {
-    public static void main(String[] args) {
+import ru.voothi.webapp.model.Resume;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class MainCollections {
+    private static final String UUID_1 = "uuid1";
+    private static final String UUID_2 = "uuid2";
+    private static final String UUID_3 = "uuid3";
+    private static final String UUID_4 = "uuid4";
+    private static final Resume RESUME_1 = new Resume(UUID_1);
+    private static final Resume RESUME_2 = new Resume(UUID_2);
+    private static final Resume RESUME_3 = new Resume(UUID_3);
+    private static final Resume RESUME_4 = new Resume(UUID_4);
+
+    public static void main(String[] args) {
+        Collection<Resume> collection = new ArrayList<>();
+        collection.add(RESUME_1);
+        collection.add(RESUME_2);
+        collection.add(RESUME_3);
+
+        Iterator<Resume> iterator = collection.iterator();
+        while (iterator.hasNext()) {
+            Resume resume = iterator.next();
+            System.out.println(resume.getUuid());
+            if (resume.getUuid() == UUID_1) {
+                iterator.remove();
+            }
+        }
+        System.out.println(collection.toString());
     }
 }
