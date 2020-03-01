b60f4aa179a5ab47c602e20e20c2299873131995
Denis Novikov <voothi@gmail.com>
2020-03-01 09:19:54 +0300

    Lesson7 - Logging   - Add java util logging

--- src/ru/voothi/webapp/storage/AbstractStorage.java
+++ src/ru/voothi/webapp/storage/AbstractStorage.java
@@ -6,32 +6,40 @@ import ru.voothi.webapp.model.Resume;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Logger;
 
 public abstract class AbstractStorage<SK> implements Storage {
+    public static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
+
     public void save(Resume resume) {
+        LOG.info("Save " + resume);
         String uuid = resume.getUuid();
         SK searchKey = getNotExistedSearchKey(uuid);
         doSave(resume, searchKey);
     }
 
     public Resume get(String uuid) {
+        LOG.info("Get " + uuid);
         SK searchKey = getExistedSearchKey(uuid);
         return doGet(searchKey);
     }
 
     public void update(Resume resume) {
+        LOG.info("Update " + resume);
         String uuid = resume.getUuid();
         SK searchKey = getExistedSearchKey(uuid);
         doUpdate(resume, searchKey);
     }
 
     public void delete(String uuid) {
+        LOG.info("Delete " + uuid);
         SK searchKey = getExistedSearchKey(uuid);
         doDelete(searchKey);
     }
 
     @Override
     public List<Resume> getAllSorted() {
+        LOG.info("getAllSorted");
         List<Resume> list = doCopyAll();
         Collections.sort(list);
         return list;
@@ -40,6 +48,7 @@ public abstract class AbstractStorage<SK> implements Storage {
     private SK getExistedSearchKey(String uuid) {
         SK searchKey = getSearchKey(uuid);
         if (!isExist(searchKey)) {
+            LOG.warning("Resume " + uuid + " not present");
             throw new NotExistStorageException(uuid);
         }
         return searchKey;
@@ -48,6 +57,7 @@ public abstract class AbstractStorage<SK> implements Storage {
     private SK getNotExistedSearchKey(String uuid) {
         SK searchKey = getSearchKey(uuid);
         if (isExist(searchKey)) {
+            LOG.warning("Resume " + uuid + " present");
             throw new ExistStorageException(uuid);
         }
         return searchKey;
