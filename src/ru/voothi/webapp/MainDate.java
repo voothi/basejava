8814b9622f7117ad702d940a29bf676ecac5dd56
Denis Novikov <voothi@gmail.com>
2020-03-03 11:31:53 +0300

    Lesson8 - Date

---
+++ src/ru/voothi/webapp/MainDate.java
@@ -0,0 +1,11 @@
+package ru.voothi.webapp;
+
+import java.util.Date;
+
+public class MainDate {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date);
+        System.out.println(date.getTime());
+    }
+}
