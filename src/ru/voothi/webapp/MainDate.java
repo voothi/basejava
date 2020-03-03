fb475c2ad0c1585b14c2518337168eae9b03d4e9
Denis Novikov <voothi@gmail.com>
2020-03-03 11:33:36 +0300

    Lesson8 - currentTimeMillis

--- src/ru/voothi/webapp/MainDate.java
+++ src/ru/voothi/webapp/MainDate.java
@@ -6,6 +6,6 @@ public class MainDate {
     public static void main(String[] args) {
         Date date = new Date();
         System.out.println(date);
-        System.out.println(date.getTime());
+        System.out.println(System.currentTimeMillis() - date.getTime());
     }
 }
