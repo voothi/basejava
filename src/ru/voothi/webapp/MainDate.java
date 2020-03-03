a0c73459a5949c2240584e6639ac0ed5760b2e47
Denis Novikov <voothi@gmail.com>
2020-03-03 13:49:27 +0300

    Lesson8 - SimpleDateFormatter - DateTimeFormatter

--- src/ru/voothi/webapp/MainDate.java
+++ src/ru/voothi/webapp/MainDate.java
@@ -1,5 +1,10 @@
 package ru.voothi.webapp;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class MainDate {
@@ -8,5 +13,18 @@ public class MainDate {
         Date date = new Date();
         System.out.println(date);
         System.out.println(System.currentTimeMillis() - start);
+
+        LocalDate ld = LocalDate.now();
+        LocalTime lt = LocalTime.now();
+        LocalDateTime ldt = LocalDateTime.now();
+        System.out.println(ld);
+        System.out.println(lt);
+        System.out.println(ldt);
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("YY/MM/dd");
+        System.out.println(simpleDateFormat.format(date));
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("YY/MM/dd");
+        System.out.println(dateTimeFormatter.format(ldt));
     }
 }
