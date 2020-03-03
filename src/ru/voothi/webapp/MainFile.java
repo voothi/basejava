a074ec93809f888a21a8f41fd9f489eb3ac2c29d
Denis Novikov <voothi@gmail.com>
2020-03-03 17:01:54 +0300

    Lesson8 - try-catch

--- src/ru/voothi/webapp/MainFile.java
+++ src/ru/voothi/webapp/MainFile.java
@@ -1,18 +1,39 @@
 package ru.voothi.webapp;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class MainFile {
-    public static void main(String[] args) throws IOException {
-        File file = new File("./.gitignore");
-        System.out.println(file.getCanonicalPath());
+    public static void main(String[] args) {
+        File filePath = new File("./.gitignore");
+        try {
+            System.out.println(filePath.getCanonicalPath());
+        } catch (IOException e) {
+            throw new RuntimeException("Error", e);
+        }
         File dir = new File("./src/ru/voothi/webapp");
         System.out.println(dir.isDirectory());
         String[] list = dir.list();
         if (list != null) {
             for (String fileName : list) {
                 System.out.println(fileName);
+            }
+        }
+
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(filePath);
+            System.out.println(fis.read());
+        } catch (IOException e) {
+            throw new RuntimeException("", e);
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
