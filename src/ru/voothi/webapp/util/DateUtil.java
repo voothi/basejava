fc37356ea26d151dbb9224fb10dfcc66a26266f9
Denis Novikov <voothi@gmail.com>
2020-03-03 14:02:14 +0300

    Lesson8 - DateUtil

---
+++ src/ru/voothi/webapp/util/DateUtil.java
@@ -0,0 +1,10 @@
+package ru.voothi.webapp.util;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+public class DateUtil {
+    public static LocalDate of(int year, Month month) {
+        return LocalDate.of(year, month, 1);
+    }
+}
