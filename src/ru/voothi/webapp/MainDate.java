76f26561c95c9b9ad9444925b52e63dc8243b51c
Denis Novikov <voothi@gmail.com>
2020-03-03 11:35:40 +0300

    Lesson8 - currentTimeMillis w/o Date

--- src/ru/voothi/webapp/MainDate.java
+++ src/ru/voothi/webapp/MainDate.java
@@ -4,8 +4,9 @@ import java.util.Date;
 
 public class MainDate {
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
         Date date = new Date();
         System.out.println(date);
-        System.out.println(System.currentTimeMillis() - date.getTime());
+        System.out.println(System.currentTimeMillis() - start);
     }
 }
