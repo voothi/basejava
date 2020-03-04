db294984ed6f0d825e303d6eecb035534c008392
Denis Novikov <voothi@gmail.com>
2020-03-04 08:02:42 +0300

    Lesson8 - Position refactoring - as static nested class

--- src/ru/voothi/webapp/model/Organization.java
+++ src/ru/voothi/webapp/model/Organization.java
@@ -1,5 +1,6 @@
 package ru.voothi.webapp.model;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -36,5 +37,22 @@ public class Organization {
     @Override
     public int hashCode() {
         return Objects.hash(homePage);
+    }
+
+    public static class Position {
+        private final LocalDate startDate;
+        private final LocalDate endDate;
+        private final String title;
+        private final String description;
+
+        public Position(LocalDate startDate, LocalDate endDate, String title, String description) {
+            Objects.requireNonNull(startDate, "startDate must not be null");
+            Objects.requireNonNull(endDate, "endDate must not be null");
+            Objects.requireNonNull(title, "title must not be null");
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.title = title;
+            this.description = description;
+        }
     }
 }
