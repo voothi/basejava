b6b76daaa8a6cc87c0c899ec160ccc90b8490c4b
Denis Novikov <voothi@gmail.com>
2020-03-06 08:01:14 +0300

    Lesson9 - AbstractPathStorage - clear()

--- src/ru/voothi/webapp/storage/AbstractPathStorage.java
+++ src/ru/voothi/webapp/storage/AbstractPathStorage.java
@@ -10,6 +10,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Consumer;
 
 public abstract class AbstractPathStorage extends AbstractStorage<Path> {
     private Path directory;
@@ -24,78 +25,58 @@ public abstract class AbstractPathStorage extends AbstractStorage<Path> {
 
     @Override
     protected Path getSearchKey(String uuid) {
-        return new Path(directory, uuid);
+
     }
 
     @Override
     protected boolean isExist(Path file) {
-        return file.exists();
+
     }
 
     @Override
     protected void doSave(Resume resume, Path file) {
-        try {
-            file.createNewPath();
-        } catch (IOException e) {
-            throw new StorageException("Couldn't create file" + file.getAbsolutePath(), file.getName(), e);
-        }
-        doUpdate(resume, file);
+
     }
 
     @Override
     protected Resume doGet(Path file) {
-        try {
-            return doRead(new BufferedInputStream(new PathInputStream(file)));
-        } catch (IOException e) {
-            throw new StorageException("Path read error", file.getName(), e);
-        }
+
     }
 
     @Override
     protected void doUpdate(Resume resume, Path file) {
-        try {
-            doWrite(resume, new BufferedOutputStream(new PathOutputStream(file)));
-        } catch (IOException e) {
-            throw new StorageException("Path write error", resume.getUuid(), e);
-        }
+
     }
 
     @Override
     protected void doDelete(Path file) {
-        if (!file.delete()) {
-            throw new StorageException("Path delete error", file.getName());
-        }
+
     }
 
     @Override
     protected List<Resume> doCopyAll() {
-        Path[] files = directory.listPaths();
-        if (files == null) {
-            throw new StorageException("Directory read error", null);
-        }
-        List<Resume> list = new ArrayList<>(files.length);
-        for (Path file : files) {
-            list.add(doGet(file));
-        }
-        return list;
+
     }
 
     @Override
     public int size() {
-        String[] list = directory.list();
-        if (list == null) {
-            throw new StorageException("Directory read error", null);
-        }
-        return list.length;
+
     }
 
     @Override
     public void clear() {
-        Path[] files = directory.listPaths();
-        if (files != null) {
-            for (Path file : files) {
-                doDelete(file);
-            }
+        try {
+            final PathConsumer action = new PathConsumer();
+            Files.list(directory).forEach(action);
+        } catch (IOException e) {
+            throw new StorageException("Path delete error", null, e);
+        }
+    }
+
+    private class PathConsumer implements Consumer<Path> {
+        @Override
+        public void accept(Path path) {
+            doDelete(path);
         }
     }
 
