a626c4c52eeacf943e43a4bdae97ba258e379eb9
Denis Novikov <voothi@gmail.com>
2020-03-12 15:15:41 +0300

    Lesson11 - Thread() @Override run()

---
+++ src/ru/voothi/webapp/MainConcurrency.java
@@ -0,0 +1,13 @@
+package ru.voothi.webapp;
+
+public class MainConcurrency {
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName());
+        new Thread() {
+            @Override
+            public void run() {
+                System.out.println(getName());
+            }
+        }.start();
+    }
+}
