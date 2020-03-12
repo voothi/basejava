6c5625ccad5b345e32f0f467626757424af20759
Denis Novikov <voothi@gmail.com>
2020-03-12 15:58:04 +0300

    Lesson11 - count - main thread

--- src/ru/voothi/webapp/MainConcurrency.java
+++ src/ru/voothi/webapp/MainConcurrency.java
@@ -1,15 +1,34 @@
 package ru.voothi.webapp;
 
 public class MainConcurrency {
+    private static int counter;
+
     public static void main(String[] args) {
         System.out.println(Thread.currentThread().getName());
-        new Thread() {
+
+        final Thread thread0 = new Thread() {
             @Override
             public void run() {
-                System.out.println(getName());
+                System.out.println(getName() + ", " + getState());
             }
-        }.start();
+        };
+        thread0.start();
 
-        new Thread(() -> System.out.println(Thread.currentThread().getName())).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState());
+            }
+        }).start();
+
+        System.out.println(thread0.getState());
+
+        for (int i = 0; i < 10000; i++) {
+            for (int j = 0; j < 100; j++) {
+                counter++;
+            }
+        }
+
+        System.out.println(counter);
     }
 }
