6fa2beb23df9428612493e8afa84186c9d936aaa
Denis Novikov <voothi@gmail.com>
2020-03-12 06:32:33 +0300

    HW10 - DataStreamSerializer null

--- src/ru/voothi/webapp/model/Organization.java
+++ src/ru/voothi/webapp/model/Organization.java
@@ -90,7 +90,7 @@ public class Organization implements Serializable {
             this.startDate = startDate;
             this.endDate = endDate;
             this.title = title;
-            this.description = description;
+            this.description = description == null ? "" : description;
         }
 
         public LocalDate getStartDate() {
