0eb91ba78ee8a7bd7900058be0895cd824ddb7cf
Denis Novikov <voothi@gmail.com>
2020-03-12 16:28:07 +0300

    Lesson11 - count - inc() and Thread.sleep

--- src/ru/voothi/webapp/MainConcurrency.java
+++ src/ru/voothi/webapp/MainConcurrency.java
@@ -3,7 +3,7 @@ package ru.voothi.webapp;
 public class MainConcurrency {
     private static int counter;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println(Thread.currentThread().getName());
 
         final Thread thread0 = new Thread() {
@@ -26,11 +26,17 @@ public class MainConcurrency {
         for (int i = 0; i < 10000; i++) {
             new Thread(() -> {
                 for (int j = 0; j < 100; j++) {
-                    counter++;
+                    inc();
                 }
             }).start();
         }
 
+        Thread.sleep(500);
+
         System.out.println(counter);
+    }
+
+    private static synchronized void inc () {
+        counter++;
     }
 }
