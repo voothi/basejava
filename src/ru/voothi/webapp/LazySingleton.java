fd7c06f122f356ec78fd67f7b04592d4d0d46ff1
Denis Novikov <voothi@gmail.com>
2020-03-13 07:54:18 +0300

    Lesson11 - Lazy Singleton Initialization-on-demand holder idiom

--- src/ru/voothi/webapp/LazySingleton.java
+++ src/ru/voothi/webapp/LazySingleton.java
@@ -1,19 +1,14 @@
 package ru.voothi.webapp;
 
 public class LazySingleton {
-    volatile public static LazySingleton INSTANCE;
-
     private LazySingleton() {
     }
 
+    private static class LazySingletonHolder {
+        private static final LazySingleton INSTANCE = new LazySingleton();
+    }
+
     public static LazySingleton getInstance() {
-        if (INSTANCE == null) {
-            synchronized (LazySingleton.class) {
-                if (INSTANCE == null) {
-                    INSTANCE = new LazySingleton();
-                }
-            }
-        }
-        return INSTANCE;
+        return LazySingletonHolder.INSTANCE;
     }
 }
