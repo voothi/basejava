fb8949b478806bf7e3aa09f075d91ba7a7b249e7
Denis Novikov <voothi@gmail.com>
2020-03-13 08:49:57 +0300

    Lesson11 - Deadlock

--- src/ru/voothi/webapp/MainConcurrency.java
+++ src/ru/voothi/webapp/MainConcurrency.java
@@ -49,6 +49,30 @@ public class MainConcurrency {
         });
 
         System.out.println(counter);
+
+        final String lock1 = "lock1";
+        final String lock2 = "lock2";
+        deadlock(lock1, lock2);
+        deadlock(lock2, lock1);
+
+    }
+
+    private static void deadlock(Object lock1, Object lock2) {
+        new Thread(() -> {
+            System.out.println("Wait " + lock1);
+            synchronized (lock1) {
+                System.out.println("Hold " + lock1);
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Wait " + lock2);
+                synchronized (lock2) {
+                    System.out.println("Hold " + lock2);
+                }
+            }
+        }).start();
     }
 
     private synchronized void inc() {
