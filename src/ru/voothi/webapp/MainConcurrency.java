7466e3dc557bdfae282b21525c88d60df47345c9
Denis Novikov <voothi@gmail.com>
2020-03-14 15:22:15 +0300

    Lesson12 - CountDownLatch

--- src/ru/voothi/webapp/MainConcurrency.java
+++ src/ru/voothi/webapp/MainConcurrency.java
@@ -1,7 +1,6 @@
 package ru.voothi.webapp;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 public class MainConcurrency {
     public static final Object LOCK = new Object();
@@ -29,50 +28,19 @@ public class MainConcurrency {
         System.out.println(thread0.getState());
 
         final MainConcurrency mainConcurrency = new MainConcurrency();
-        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+        CountDownLatch countDownLatch = new CountDownLatch(THREADS_NUMBER);
         for (int i = 0; i < THREADS_NUMBER; i++) {
             final Thread thread = new Thread(() -> {
                 for (int j = 0; j < 100; j++) {
                     mainConcurrency.inc();
                 }
+                countDownLatch.countDown();
             });
             thread.start();
-            threads.add(thread);
         }
 
-        threads.forEach(t -> {
-            try {
-                t.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-
+        countDownLatch.await();
         System.out.println(counter);
-
-        final String lock1 = "lock1";
-        final String lock2 = "lock2";
-        deadlock(lock1, lock2);
-        deadlock(lock2, lock1);
-
-    }
-
-    private static void deadlock(Object lock1, Object lock2) {
-        new Thread(() -> {
-            System.out.println("Wait " + lock1);
-            synchronized (lock1) {
-                System.out.println("Hold " + lock1);
-                try {
-                    Thread.sleep(50);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("Wait " + lock2);
-                synchronized (lock2) {
-                    System.out.println("Hold " + lock2);
-                }
-            }
-        }).start();
     }
 
     private synchronized void inc() {
