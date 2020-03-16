803a686e4c877a357f567b747f9fa2b35b946552
Denis Novikov <voothi@gmail.com>
2020-03-16 11:53:13 +0300

    Lesson13 - err is

--- src/ru/voothi/webapp/Config.java
+++ src/ru/voothi/webapp/Config.java
@@ -1,19 +1,31 @@
 package ru.voothi.webapp;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class Config {
     private static final Config INSTANCE = new Config();
     private static final File PROPS = new File("./config/resumes.properties");
+    private Properties props = new Properties();
+    private File storageDir;
 
     private Config() {
         try (InputStream is = new FileInputStream(PROPS)) {
+            props.load(is);
+            storageDir = new File(props.getProperty("storage.dir"));
         } catch (IOException e) {
-            throw new IllegalStateException("Invalid config " + PROPS);
+            throw new IllegalStateException("Invalid config " + PROPS.getAbsolutePath());
         }
     }
 
     public static Config getInstance() {
         return INSTANCE;
+    }
+
+    public File getStorageDir() {
+        return storageDir;
     }
 }
--- test/ru/voothi/webapp/storage/AbstractStorageTest.java
+++ test/ru/voothi/webapp/storage/AbstractStorageTest.java
@@ -2,19 +2,21 @@ package ru.voothi.webapp.storage;
 
 import org.junit.Before;
 import org.junit.Test;
+import ru.voothi.webapp.Config;
 import ru.voothi.webapp.exception.ExistStorageException;
 import ru.voothi.webapp.exception.NotExistStorageException;
-import ru.voothi.webapp.model.*;
+import ru.voothi.webapp.model.ContactType;
+import ru.voothi.webapp.model.Resume;
 
 import java.io.File;
-import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
 
-import static junit.framework.TestCase.*;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
 
 public abstract class AbstractStorageTest {
-    protected static final File STORAGEDIR = new File("./storage");
+    protected static final File STORAGEDIR = Config.getInstance().getStorageDir();
 
     private static final String UUID_1 = "uuid1";
     private static final String UUID_2 = "uuid2";
@@ -32,38 +34,38 @@ public abstract class AbstractStorageTest {
         RESUME_3 = new Resume(UUID_3, "fullName3");
         RESUME_4 = new Resume(UUID_4, "fullName4");
 
-        RESUME_1.addContact(ContactType.PHONE, "+7(921) 855-0482");
-        RESUME_1.addContact(ContactType.SKYPE, "grigory.kislin");
-        RESUME_1.addContact(ContactType.EMAIL, "gkislin@yandex.ru");
-        RESUME_1.addContact(ContactType.LINKEDIN, "https://www.linkedin.com/in/gkislin");
-        RESUME_1.addContact(ContactType.GITHUB, "https://github.com/gkislin");
-        RESUME_1.addContact(ContactType.STACKOVERFLOW, "https://stackoverflow.com/users/548473");
-        RESUME_1.addContact(ContactType.HOME_PAGE, "http://gkislin.ru/");
-        RESUME_1.addSection(SectionType.OBJECTIVE, new TextSection("Ведущий стажировок"));
-        RESUME_1.addSection(SectionType.PERSONAL, new TextSection("Аналитический склад ума"));
-        RESUME_1.addSection(SectionType.ACHIEVEMENT, new ListSection("С 2013 года: разработка проектов",
-                "Реализация двухфакторной аутентификации"));
-        RESUME_1.addSection(SectionType.QUALIFICATION, new ListSection("JEE AS: GlassFish",
-                "Version control:"));
-        RESUME_1.addSection(SectionType.EXPERIENCE,
-                new OrganizationSection(
-                        new Organization("Java Online Projects", "http://javaops.ru/",
-                                new Organization.Position(
-                                        2013, Month.OCTOBER,
-                                        "Автор проекта.", "Создание, организация"))));
-        RESUME_1.addSection(SectionType.EDUCATION,
-                new OrganizationSection(
-                        new Organization("Санкт-Петербургский национальный исследовательский университет " +
-                                "информационных технологий, механики и оптики",
-                                "http://www.ifmo.ru/",
-                                new Organization.Position(
-                                        1993, Month.SEPTEMBER,
-                                        1996, Month.JULY,
-                                        "Аспирантура (программист С, С++)", ""),
-                                new Organization.Position(
-                                        1987, Month.SEPTEMBER,
-                                        1993, Month.JULY,
-                                        "Инженер (программист Fortran, C)", ""))));
+//        RESUME_1.addContact(ContactType.PHONE, "+7(921) 855-0482");
+//        RESUME_1.addContact(ContactType.SKYPE, "grigory.kislin");
+//        RESUME_1.addContact(ContactType.EMAIL, "gkislin@yandex.ru");
+//        RESUME_1.addContact(ContactType.LINKEDIN, "https://www.linkedin.com/in/gkislin");
+//        RESUME_1.addContact(ContactType.GITHUB, "https://github.com/gkislin");
+//        RESUME_1.addContact(ContactType.STACKOVERFLOW, "https://stackoverflow.com/users/548473");
+//        RESUME_1.addContact(ContactType.HOME_PAGE, "http://gkislin.ru/");
+//        RESUME_1.addSection(SectionType.OBJECTIVE, new TextSection("Ведущий стажировок"));
+//        RESUME_1.addSection(SectionType.PERSONAL, new TextSection("Аналитический склад ума"));
+//        RESUME_1.addSection(SectionType.ACHIEVEMENT, new ListSection("С 2013 года: разработка проектов",
+//                "Реализация двухфакторной аутентификации"));
+//        RESUME_1.addSection(SectionType.QUALIFICATION, new ListSection("JEE AS: GlassFish",
+//                "Version control:"));
+//        RESUME_1.addSection(SectionType.EXPERIENCE,
+//                new OrganizationSection(
+//                        new Organization("Java Online Projects", "http://javaops.ru/",
+//                                new Organization.Position(
+//                                        2013, Month.OCTOBER,
+//                                        "Автор проекта.", "Создание, организация"))));
+//        RESUME_1.addSection(SectionType.EDUCATION,
+//                new OrganizationSection(
+//                        new Organization("Санкт-Петербургский национальный исследовательский университет " +
+//                                "информационных технологий, механики и оптики",
+//                                "http://www.ifmo.ru/",
+//                                new Organization.Position(
+//                                        1993, Month.SEPTEMBER,
+//                                        1996, Month.JULY,
+//                                        "Аспирантура (программист С, С++)", ""),
+//                                new Organization.Position(
+//                                        1987, Month.SEPTEMBER,
+//                                        1993, Month.JULY,
+//                                        "Инженер (программист Fortran, C)", ""))));
 
         RESUME_2.addContact(ContactType.PHONE, "+7(999) 999-99999");
         RESUME_2.addContact(ContactType.SKYPE, "skype_contact2");
