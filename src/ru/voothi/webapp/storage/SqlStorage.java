7596f9ed9a230c4e3d2bb79352ad7fedce6e5aef
Denis Novikov <voothi@gmail.com>
2020-03-20 22:16:24 +0300

    Lesson13 - ConnectionFactory

--- src/ru/voothi/webapp/storage/SqlStorage.java
+++ src/ru/voothi/webapp/storage/SqlStorage.java
@@ -1,10 +1,25 @@
 package ru.voothi.webapp.storage;
 
 import ru.voothi.webapp.model.Resume;
+import ru.voothi.webapp.sql.ConnectionFactory;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SqlStorage implements Storage {
+    ConnectionFactory connectionFactory;
+
+    public SqlStorage(String dbUrl, String dbUser, String dbPassword) {
+        connectionFactory = new ConnectionFactory() {
+            @Override
+            public Connection getConnection() throws SQLException {
+                return DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+            }
+        };
+    }
+
     @Override
     public void save(Resume resume) {
 
