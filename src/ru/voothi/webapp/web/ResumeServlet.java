6c1e8c6fc7de0525ec1a7b215b51dff178b40fca
Denis Novikov <voothi@gmail.com>
2020-04-02 21:13:35 +0300

    Lesson15 - ResumeServlet

--- src/ru/voothi/webapp/web/ResumeServlet.java
+++ src/ru/voothi/webapp/web/ResumeServlet.java
@@ -11,11 +11,10 @@ public class ResumeServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setHeader("Content-Type", "text/html; charset=UTF-8");
         String name = request.getParameter("name");
-        if (name == null) {
-            response.getWriter().write("Hello resumes");
-        } else {
-            response.getWriter().write("Hello! " + name + '!');
-        }
+        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
     }
 }
