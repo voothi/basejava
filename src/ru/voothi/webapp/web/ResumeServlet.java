e9713961ac60f406a90e85bc381c305bea0ef291
Denis Novikov <voothi@gmail.com>
2020-04-04 16:18:01 +0300

    Lesson16 - Impl view, delete, edit in progress

--- src/ru/voothi/webapp/web/ResumeServlet.java
+++ src/ru/voothi/webapp/web/ResumeServlet.java
@@ -1,6 +1,7 @@
 package ru.voothi.webapp.web;
 
 import ru.voothi.webapp.Config;
+import ru.voothi.webapp.model.Resume;
 import ru.voothi.webapp.storage.Storage;
 
 import javax.servlet.ServletConfig;
@@ -24,7 +25,29 @@ public class ResumeServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        request.setAttribute("resumes", storage.getAllSorted());
-        request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
+        String uuid = request.getParameter("uuid");
+        String action = request.getParameter("action");
+        if (action == null) {
+            request.setAttribute("resumes", storage.getAllSorted());
+            request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
+            return;
+        }
+        Resume resume;
+        switch (action) {
+            case "delete":
+                storage.delete(uuid);
+                response.sendRedirect("resume");
+                return;
+            case "view":
+            case "edit":
+                resume = storage.get(uuid);
+                break;
+            default:
+                throw new IllegalArgumentException("Action " + action + " is illegal");
+        }
+        request.setAttribute("resume", resume);
+        request.getRequestDispatcher(
+                ("view".equals(action) ? "/WEB-INF/jsp/view.jsp" : "/WEB-INF/jsp/edit.jsp")
+        ).forward(request, response);
     }
 }
